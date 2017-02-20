0d8a33d8028a2b933ece6d19adb82490f85604c4
Swizec Teller <swizec@swizec.com>
2017-02-19 16:25:26 -0800

    Zoomable pannable map

--- src/Maps.js
+++ src/Maps.js
@@ -68,12 +68,18 @@ const Migrations = ({ topology, projection, data, nameIdMap, focusCountry }) => 
 
 class World extends Component {
     state = {
-        topology: null
+        topology: null,
+        zoomInitted: false,
+        transform: null
     }
 
     projection = d3.geoEquirectangular()
                    .center([-50, 40])
-                   .scale(200)
+                   .scale(200);
+
+    zoom = d3.zoom()
+             .scaleExtent([.5, 10])
+             .on('zoom', this.onZoom.bind(this));
 
     componentWillMount() {
         d3.json('data/world-110m.v1.json',
@@ -82,6 +88,33 @@ class World extends Component {
                         topology: topology
                     });
                 });
+    }
+
+    componentDidUpdate() {
+        if (!this.state.zoomInitted && this.state.topology) {
+            const svg = d3.select(this.refs.svg);
+
+            svg.call(this.zoom);
+
+            this.setState({
+                zoomInitted: true
+            });
+        }
+    }
+
+    onZoom() {
+        this.setState({
+            transform: d3.event.transform
+        });
+    }
+
+    get transform() {
+        if (this.state.transform) {
+            const { x, y, k } = this.state.transform;
+            return `translate(${x}, ${y}) scale(${k})`;
+        }else{
+            return null;
+        }
     }
 
     render() {
@@ -93,12 +126,14 @@ class World extends Component {
         }
 
         return (
-            <svg width={width} height={height}>
-                <Map topology={topology} projection={this.projection} data={this.props.data}
-                     nameIdMap={this.props.nameIdMap} focusCountry={this.props.focusCountry} />
-                <Migrations topology={topology} projection={this.projection}
-                            data={this.props.data} nameIdMap={this.props.nameIdMap}
-                            focusCountry={this.props.focusCountry} />
+            <svg width={width} height={height} ref="svg">
+                <g transform={this.transform}>
+                    <Map topology={topology} projection={this.projection} data={this.props.data}
+                         nameIdMap={this.props.nameIdMap} focusCountry={this.props.focusCountry} />
+                    <Migrations topology={topology} projection={this.projection}
+                                data={this.props.data} nameIdMap={this.props.nameIdMap}
+                                focusCountry={this.props.focusCountry} />
+                </g>
             </svg>
         )
     }
