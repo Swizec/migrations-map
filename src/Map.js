19cd393f7007860f71ce886c3830031f5aeb8dd5
Swizec Teller <swizec@swizec.com>
2018-01-07 15:15:22 -0800

    Fix yarn install and bug when data=null

--- src/Map.js
+++ src/Map.js
@@ -1,60 +1,62 @@
-
-import React from 'react';
-import * as d3 from 'd3';
-import * as topojson from 'topojson-client';
-import _ from 'lodash';
-
+import React from "react";
+import * as d3 from "d3";
+import * as topojson from "topojson-client";
+import _ from "lodash";
 
 function migrationSources(data, centroids, nameIdMap) {
     return Object.keys(data.sources)
-                 .filter(name => centroids[nameIdMap[name]])
-                 .filter(name => data.sources[name] !== 0)
+        .filter(name => centroids[nameIdMap[name]])
+        .filter(name => data.sources[name] !== 0);
 }
-
 
 const Map = ({ topology, projection, data, nameIdMap, focusCountry }) => {
     const D = d3.geoPath(projection),
-          countries = topojson.feature(topology, topology.objects.countries),
-          centroids = _.fromPairs(countries.features
-                                           .map(country => [country.id,
-                                                            D.centroid(country)])),
-          idNameMap = _.invert(nameIdMap);
+        countries = topojson.feature(topology, topology.objects.countries),
+        centroids = _.fromPairs(
+            countries.features.map(country => [country.id, D.centroid(country)])
+        ),
+        idNameMap = _.invert(nameIdMap);
+
+    console.log(data);
 
     let sources = [],
         focusData = data.find(({ id }) => id === focusCountry),
         colorScale = _.noop;
 
     if (focusData) {
-        sources = migrationSources(focusData,
-                                   centroids, nameIdMap);
-        colorScale = d3.scaleLog()
-                       .domain(d3.extent(sources.map(name => focusData.sources[name])))
-                       .range([0, 1]);
+        sources = migrationSources(focusData, centroids, nameIdMap);
+        colorScale = d3
+            .scaleLog()
+            .domain(d3.extent(sources.map(name => focusData.sources[name])))
+            .range([0, 1]);
     }
 
-    const isSource = (country) => sources.includes(idNameMap[country.id]),
-          color = (country) => {
-              if (isSource(country) && focusData){
-                  return d3.interpolateWarm(colorScale(
-                      focusData.sources[idNameMap[country.id]]
-                  ));
-              }else{
-                  return 'grey';
-              }
-          };
-
+    const isSource = country => sources.includes(idNameMap[country.id]),
+        color = country => {
+            if (isSource(country) && focusData) {
+                return d3.interpolateWarm(
+                    colorScale(focusData.sources[idNameMap[country.id]])
+                );
+            } else {
+                return "grey";
+            }
+        };
 
     return (
-       <g>
-           {countries.features.map((country, i) => (
-               <path d={D(country)}
-                     key={`${country.id}-${i}`}
-                     style={{stroke: 'white',
-                             strokeWidth: '0.25px',
-                             fillOpacity: isSource(country) ? 1 : 0.5,
-                             fill: color(country)}} />
+        <g>
+            {countries.features.map((country, i) => (
+                <path
+                    d={D(country)}
+                    key={`${country.id}-${i}`}
+                    style={{
+                        stroke: "white",
+                        strokeWidth: "0.25px",
+                        fillOpacity: isSource(country) ? 1 : 0.5,
+                        fill: color(country)
+                    }}
+                />
             ))}
-       </g>
+        </g>
     );
 };
 
--- src/Maps.js
+++ src/Maps.js
@@ -1,66 +1,80 @@
+import React, { Component } from "react";
+import * as d3 from "d3";
+import * as topojson from "topojson-client";
+import _ from "lodash";
 
-import React, { Component } from 'react';
-import * as d3 from 'd3';
-import * as topojson from 'topojson-client';
-import _ from 'lodash';
-
-import Map from './Map';
-import MigrationLine from './MigrationLine';
+import Map from "./Map";
+import MigrationLine from "./MigrationLine";
 
 function migrationSources(data, centroids, nameIdMap) {
     return Object.keys(data.sources)
-                 .filter(name => centroids[nameIdMap[name]])
-                 .filter(name => data.sources[name] !== 0)
+        .filter(name => centroids[nameIdMap[name]])
+        .filter(name => data.sources[name] !== 0);
 }
-
 
 const CountryMigrations = ({ data, nameIdMap, centroids }) => {
     const destination = centroids[data.id];
 
     const sources = migrationSources(data, centroids, nameIdMap),
-          extent = d3.extent(sources.map(name => data.sources[name])),
-          color = d3.scaleLog()
-                    .domain(extent)
-                    .range([0, 1]);
+        extent = d3.extent(sources.map(name => data.sources[name])),
+        color = d3
+            .scaleLog()
+            .domain(extent)
+            .range([0, 1]);
 
-    const N = d3.scaleLinear()
-                .domain(extent)
-                .range([1, 10]);
+    const N = d3
+        .scaleLinear()
+        .domain(extent)
+        .range([1, 10]);
 
     return (
         <g>
             {sources.map((name, i) => (
-                <MigrationLine start={centroids[nameIdMap[name]]}
-                               end={destination}
-                               color={d3.interpolateWarm(color(data.sources[name]))}
-                               key={`${data.id}-${i}`}
-                               Ncircles={N(data.sources[name])}
+                <MigrationLine
+                    start={centroids[nameIdMap[name]]}
+                    end={destination}
+                    color={d3.interpolateWarm(color(data.sources[name]))}
+                    key={`${data.id}-${i}`}
+                    Ncircles={N(data.sources[name])}
                 />
-             ))}
+            ))}
         </g>
-    )
+    );
 };
 
-const Migrations = ({ topology, projection, data, nameIdMap, focusCountry }) => {
+const Migrations = ({
+    topology,
+    projection,
+    data,
+    nameIdMap,
+    focusCountry
+}) => {
     if (!data) {
         return null;
     }
 
     const countries = topojson.feature(topology, topology.objects.countries),
-          path = d3.geoPath(projection),
-          centroids = _.fromPairs(countries.features
-                                           .map(country => [country.id,
-                                                            path.centroid(country)]));
+        path = d3.geoPath(projection),
+        centroids = _.fromPairs(
+            countries.features.map(country => [
+                country.id,
+                path.centroid(country)
+            ])
+        );
 
-    const dataToDraw = data.filter(({ id }) => id === focusCountry)
-                           .filter(({ id }) => !!centroids[id]);
+    const dataToDraw = data
+        .filter(({ id }) => id === focusCountry)
+        .filter(({ id }) => !!centroids[id]);
 
     return (
         <g>
             {dataToDraw.map(data => (
-                <CountryMigrations data={data} nameIdMap={nameIdMap}
-                                   centroids={centroids}
-                                   key={`migrations-${data.id}`} />
+                <CountryMigrations
+                    data={data}
+                    nameIdMap={nameIdMap}
+                    centroids={centroids}
+                    key={`migrations-${data.id}`}
+                />
             ))}
         </g>
     );
@@ -71,23 +85,24 @@ class World extends Component {
         topology: null,
         zoomInitted: false,
         transform: null
-    }
+    };
 
-    projection = d3.geoEquirectangular()
-                   .center([-50, 40])
-                   .scale(200);
+    projection = d3
+        .geoEquirectangular()
+        .center([-50, 40])
+        .scale(200);
 
-    zoom = d3.zoom()
-             .scaleExtent([.5, 10])
-             .on('zoom', this.onZoom.bind(this));
+    zoom = d3
+        .zoom()
+        .scaleExtent([0.5, 10])
+        .on("zoom", this.onZoom.bind(this));
 
     componentWillMount() {
-        d3.json('data/world-110m.v1.json',
-                (err, topology) => {
-                    this.setState({
-                        topology: topology
-                    });
-                });
+        d3.json("data/world-110m.v1.json", (err, topology) => {
+            this.setState({
+                topology: topology
+            });
+        });
     }
 
     componentDidUpdate() {
@@ -112,30 +127,39 @@ class World extends Component {
         if (this.state.transform) {
             const { x, y, k } = this.state.transform;
             return `translate(${x}, ${y}) scale(${k})`;
-        }else{
+        } else {
             return null;
         }
     }
 
     render() {
-        const { width, height } = this.props,
-              { topology } = this.state;
+        const { width, height, data } = this.props,
+            { topology } = this.state;
 
-        if (!topology) {
+        if (!topology || !data) {
             return null;
         }
 
         return (
             <svg width={width} height={height} ref="svg">
                 <g transform={this.transform}>
-                    <Map topology={topology} projection={this.projection} data={this.props.data}
-                         nameIdMap={this.props.nameIdMap} focusCountry={this.props.focusCountry} />
-                    <Migrations topology={topology} projection={this.projection}
-                                data={this.props.data} nameIdMap={this.props.nameIdMap}
-                                focusCountry={this.props.focusCountry} />
+                    <Map
+                        topology={topology}
+                        projection={this.projection}
+                        data={this.props.data}
+                        nameIdMap={this.props.nameIdMap}
+                        focusCountry={this.props.focusCountry}
+                    />
+                    <Migrations
+                        topology={topology}
+                        projection={this.projection}
+                        data={this.props.data}
+                        nameIdMap={this.props.nameIdMap}
+                        focusCountry={this.props.focusCountry}
+                    />
                 </g>
             </svg>
-        )
+        );
     }
 }
 
